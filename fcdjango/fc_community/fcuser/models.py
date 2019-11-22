393a61f59f11f763c1889c26a44a8cb18875d6cd
djangojeng-e <headfat1218@gmail.com>
2019-11-22 22:38:49 +0900

    Added __str__ to return username in string.

--- fcdjango/fc_community/fcuser/models.py
+++ fcdjango/fc_community/fcuser/models.py
@@ -7,6 +7,9 @@ class Fcuser(models.Model):
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttn = models.DateTimeField(auto_now_add=True, verbose_name="등록타임")
 
+    def __Str__(self):
+        return self.username
+
     class Meta: 
         db_table = 'fastcampus_fcuser'
 
