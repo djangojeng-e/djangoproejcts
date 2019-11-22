aef22c91bf06514e85d2494c8581c31a0c05bee9
djangojeng-e <headfat1218@gmail.com>
2019-11-22 22:19:28 +0900

    Added models. Added 'username', 'password' and 'registered_dttn'.

---
+++ fcdjango/fc_community/fcuser/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Fcuser(models.Model): 
+    username = models.CharField(max_length=64, verbose_name='사용자명')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    registered_dttn = models.DateTimeField(auto_now_add=True, verbose_name="등록타임")
+
+
+    
+
