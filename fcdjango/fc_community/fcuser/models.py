bdc3137a942988c441eaeb196750537ba9e04142
djangojeng-e <headfat1218@gmail.com>
2019-11-22 22:55:17 +0900

    Added meta verbose_name and verbose_name_plural to display user group in Korean Language.

--- fcdjango/fc_community/fcuser/models.py
+++ fcdjango/fc_community/fcuser/models.py
@@ -7,10 +7,13 @@ class Fcuser(models.Model):
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttn = models.DateTimeField(auto_now_add=True, verbose_name="등록타임")
 
-    def __Str__(self):
+    def __str__(self):
         return self.username
+
 
     class Meta: 
         db_table = 'fastcampus_fcuser'
+        verbose_name = "패캠 사용자"
+        verbose_name_plural = "패캠 사용자"
 
 
