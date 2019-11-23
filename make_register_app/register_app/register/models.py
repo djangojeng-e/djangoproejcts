e036ce5e52254a9ee16cf902151bb1eaaa0134de
djangojeng-e <headfat1218@gmail.com>
2019-11-23 18:33:31 +0900

    Added __str__ function to return username in string.

--- make_register_app/register_app/register/models.py
+++ make_register_app/register_app/register/models.py
@@ -7,6 +7,8 @@ class Django_user(models.Model):
     password = models.CharField(max_length=64, verbose_name="비밀번호")
     registered_date = models.DateTimeField(auto_now=True, verbose_name="등록타임")
 
+    def __str__(self):
+        return self.username
 
     class Meta:
         db_table = 'Djangojenge_user'
