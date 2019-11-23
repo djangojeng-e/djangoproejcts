dcce97b5e9362f022c071cd4c1f319e243b92ee3
djangojeng-e <headfat1218@gmail.com>
2019-11-23 15:42:23 +0900

    Started adding email field into the register page.

--- fcdjango/fc_community/fcuser/models.py
+++ fcdjango/fc_community/fcuser/models.py
@@ -6,7 +6,8 @@ class Fcuser(models.Model):
     username = models.CharField(max_length=64, verbose_name='사용자명')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttn = models.DateTimeField(auto_now_add=True, verbose_name="등록타임")
-
+    useremail = models.EmailField(max_length=128, verbose_name="사용자이메일")
+    
     def __str__(self):
         return self.username
 
