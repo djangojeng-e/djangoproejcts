37eeb7fe54c33ec429629515d234069c6643daad
djangojeng-e <headfat1218@gmail.com>
2019-11-27 22:32:29 +0900

    Added get() methods to each POST data in order to prevent the users putting empty fields.
    POST DATA forms are equivalent to dictionary format.
    
    If there is no key, there will be an error. We use get() method to
    create the default value.
    
    This is to prevent the users enter empty fields.

--- fcdjango/fc_community/fcuser/views.py
+++ fcdjango/fc_community/fcuser/views.py
@@ -8,9 +8,11 @@ def register(request):
     if request.method == "GET":
         return render(request, 'register.html')
     elif request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        re_password = request.POST['re-password']
+        username = request.POST.get['username', None]
+        useremail = request.POST.get['useremail', None]
+        password = request.POST.get['password', None]
+        re_password = request.POST['re-password', None]
+        
         res_data = {}
 
         if not (username and password and re_password):
@@ -19,7 +21,8 @@ def register(request):
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             fcuser = Fcuser(
-                username=username,
+                username = username,
+                useremail = useremail,
                 password = make_password(password) 
             )
 
