1068c09b9174e93a04f43e9314325bf19ee56d46
djangojeng-e <headfat1218@gmail.com>
2019-12-02 22:58:04 +0900

    Found an error on def vote. Fixed the code.

--- polls/mysite/polls/views.py
+++ polls/mysite/polls/views.py
@@ -23,9 +23,9 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
-def vote(request, question_id): 
-    def vote(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
