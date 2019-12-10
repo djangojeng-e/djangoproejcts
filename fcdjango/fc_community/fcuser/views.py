0ed4ebd3f82ab4b83c4afca1579c78eb1c52fc1c
djangojeng-e <headfat1218@gmail.com>
2019-12-10 21:54:24 +0900

    Fixed error on get() method.

--- fcdjango/fc_community/fcuser/views.py
+++ fcdjango/fc_community/fcuser/views.py
@@ -6,18 +6,18 @@ from django.contrib.auth.hashers import make_password
 
 def register(request):
     if request.method == "GET":
-        return render(request, 'register.html')
+        return render(request, 'register.html') # 경로는 template 폴더를 바라보고 있기때문에 자동으로 찾음
     elif request.method == "POST":
-        username = request.POST.get['username', None]
-        useremail = request.POST.get['useremail', None]
-        password = request.POST.get['password', None]
-        re_password = request.POST['re-password', None]
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('repassword', None)
         
         res_data = {}
 
         if not (username and password and re_password):
             res_data['error'] = "모든필드를 입력해야 합니다."
-        elif 'password' != 're-password':
+        elif 'password' != 'repassword':
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             fcuser = Fcuser(
