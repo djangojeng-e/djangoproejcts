2333523ce86ba75a8a9c0b994bf5516ae319b9a8
djangojeng-e <headfat1218@gmail.com>
2019-12-11 10:21:47 +0900

    Fixed the error and tested working fine.

--- fcdjango/fc_community/fcuser/views.py
+++ fcdjango/fc_community/fcuser/views.py
@@ -11,13 +11,13 @@ def register(request):
         username = request.POST.get('username', None)
         useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
-        re_password = request.POST.get('repassword', None)
+        repassword = request.POST.get('repassword', None)
         
         res_data = {}
 
-        # if not (username and password and re_password):
+        # if not (username and password and repassword):
         #     res_data['error'] = "모든필드를 입력해야 합니다."
-        if 'password' != 'repassword':
+        if password != repassword:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             fcuser = Fcuser(
