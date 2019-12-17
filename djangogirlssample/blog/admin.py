b33312126f7883e50eb9192b40325b689eeab820
djangojeng-e <headfat1218@gmail.com>
2019-12-17 22:15:58 +0900

    Set the basic view and set up url.

--- djangogirlssample/blog/admin.py
+++ djangogirlssample/blog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin)
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin)
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin)
+
+
+# from .models import Post
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Post)
+
+
+
--- djangogirlssample/config/urls.py
+++ djangogirlssample/config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import post_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # post-list라는 url에 온 요청은
+    # blog.views.post_list 함수가 처리한다.
+    path('post-list/', post_list),
 ]
