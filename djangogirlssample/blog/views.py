c11d6ec5a2971bfd6fc8477cced765ba18c6ac9b
djangojeng-e <headfat1218@gmail.com>
2019-12-17 22:56:57 +0900

    created templates and rendered it into post_list.html

--- djangogirlssample/blog/views.py
+++ djangogirlssample/blog/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import os
 # Create your views here.
 
 
 def post_list(request):
-    return HttpResponse("Post List!")
+    # 상위폴더(blog) 의 상위폴더 (djangogirls)의
+    # 하위폴더에 (templates)
+    # 하위파일 (post_list.html)의 내용을 read() 한 결과를
+    # httpresponse 에 인자로 전달
+
+    cur_file_path = os.path.abspath(__file__)
+    parent_dir_path = os.path.dirname(cur_file_path)
+    root_dir_path = os.path.dirname(parent_dir_path)
+    templates_dir_path = os.path.join(root_dir_path, 'templates')
+    post_list_html_path = os.path.join(templates_dir_path,'post_list.html')
+
+    f = open(post_list_html_path, 'rt')
+    html = f.read()
+    f.close()
+
+    return HttpResponse(html)
