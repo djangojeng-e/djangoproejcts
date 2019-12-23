5048d3df9f9c08d1caf4d7f72783f33c0f06421d
djangojeng-e <headfat1218@gmail.com>
2019-12-23 12:39:54 +0900

    board_write has been added to its html and in the view.

--- fcdjango/fc_community/board/views.py
+++ fcdjango/fc_community/board/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
+from .forms import Boardform
+from fcuser.models import Fcuser
 
 # Create your views here.
 
 def board_write(request):
-    form = BoardForm()
+    if request.method == 'POST':
+        form = Boardform(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            fcuser = Fcuser.objects.get(pk=user_id)
+            board = Board()
+            board.title = form.cleaned_data('title')
+            board.contents = form.cleaned_data('contents')
+            board.writer = fcuser
+            board.save()
+
+            return redirect('/board/list/')
+    else:     
+        form = Boardform()
     return render(request, 'board_write.html', {'form': form})
 
 
