2b55add28422aef3caf9d4cac4bce70c00773094
djangojeng-e <headfat1218@gmail.com>
2019-12-28 12:14:27 +0900

    Added bookmark/urls.py connected to settings/urls.py. views.py had to be added in order to register the URLs.

--- bookmark_app/bookmark/models.py
+++ bookmark_app/bookmark/models.py
@@ -7,3 +7,5 @@ class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('site URL')
 
+    def __str__(self):
+        return f'사이트명 {self.site_name} URL {self.url}'
--- bookmark_app/bookmark/views.py
+++ bookmark_app/bookmark/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Bookmark
+
 
 # Create your views here.
+
+class BookmarkListView(ListView):
+    model = Bookmark
