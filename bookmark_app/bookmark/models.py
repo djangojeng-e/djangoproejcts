933f3cdb957c134ee477a2f482b1369001ec53c0
djangojeng-e <headfat1218@gmail.com>
2019-12-28 13:10:37 +0900

    Added get_absolute_url to accomodate update function.

--- bookmark_app/bookmark/models.py
+++ bookmark_app/bookmark/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
 
 
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('site URL')
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
+
+    # pointing back to detail / self.id
 
     def __str__(self):
         return f'사이트명 {self.site_name} URL {self.url}'
