3bd234660b76006197ca0211c276003f24afcf4e
djangojeng-e <headfat1218@gmail.com>
2019-12-29 16:00:59 +0900

    Configured URLs between project and app urls.

---
+++ doostagram/photo/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import DetailView
+
+from .views import *
+
+app_name = 'photo'
+
+
+urlpatterns = [
+  path('', photo_list, name='photo_list'),
+  path('detail/<int:pk>/', DetailView.as_view(model=Photo, template_name='photo/detail.html'), name='photo_detail'),
+  path('upload/', PhotoUpdateView.as_view(), name='photo_upload'),
+  path('delete/<int:pk>', PhotoUpdateView.as_view(), name='photo_delete'),
+  path('update/<int:pk>', PhotoUpdateView.as_view(), name='photo_update'),
+
+
+]
