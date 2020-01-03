4ceda990e07cc7f0e4c4b7c1dd5ebd049f453439
djangojeng-e <headfat1218@gmail.com>
2020-01-03 15:01:47 +0900

    Created models for product and fixed models for Users.

--- shopping_mall/shopping/product/models.py
+++ shopping_mall/shopping/product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, verbose_name='상품명')
+    price = models.IntegerField(verbose_name='상품가격')
+    description = models.TextField(verbose_name='상품설명')
+    stock = models.IntegerField(verbose_name='재고')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
