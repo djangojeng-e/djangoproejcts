a0288e88b454ce5af8995b7e2929bc15463f4cc1
djangojeng-e <headfat1218@gmail.com>
2020-01-03 15:12:38 +0900

    Makemigrations and migrate to create DB.

--- shopping_mall/shopping/order/models.py
+++ shopping_mall/shopping/order/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Order(models.Model):
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='사용자')
+    user = models.ForeignKey('users.Users', on_delete=models.CASCADE, verbose_name='사용자')
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
     quantity = models.IntegerField(verbose_name='수량')
     register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
