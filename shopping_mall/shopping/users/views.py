0cde537f798d4e1e2133fa150c2cde0b55dd7eb4
djangojeng-e <headfat1218@gmail.com>
2020-01-03 15:54:50 +0900

    Created register.html and forms.py in users to make registration page.

--- shopping_mall/shopping/users/views.py
+++ shopping_mall/shopping/users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
 # Create your views here.
+from .forms import RegisterForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
 
