f36951a3fae3bd1becac3a937ae48ac494e4439e
djangojeng-e <headfat1218@gmail.com>
2020-01-10 13:22:56 +0900

    Added product page and activated humanize app in setting.

--- shopping_mall/shopping/product/views.py
+++ shopping_mall/shopping/product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Product
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product_list'
