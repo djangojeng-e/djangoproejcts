63f03631425763e21ff91d1455fde49ba72a0e74
djangojeng-e <headfat1218@gmail.com>
2020-01-11 20:12:51 +0900

    Product Detail page has been constructed.

--- shopping_mall/shopping/product/views.py
+++ shopping_mall/shopping/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 
 
@@ -20,3 +20,9 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterFrom
     success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
+    context_object_name = 'product'
