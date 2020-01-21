e61f893306bb9927cbad92d3388c7f64da941ec6
djangojeng-e <headfat1218@gmail.com>
2020-01-21 21:11:57 +0900

    4.3 ManyToMany like a Boss

--- airbnb_clone/rooms/admin.py
+++ airbnb_clone/rooms/admin.py
@@ -4,6 +4,11 @@ from . import models
 # Create your models here.
 
 
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     pass
