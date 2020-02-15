6933c9b80c0ffa618805cfe775c7db23d6e9830c
djangojeng-e <headfat1218@gmail.com>
2020-02-15 23:51:08 +0900

    modified views.py

--- DRF_Tutorial/app/snippets/views.py
+++ DRF_Tutorial/app/snippets/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
