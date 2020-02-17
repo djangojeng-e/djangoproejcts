d2c8f5634996f0be8cfc27241f0df82f5c42bb40
djangojeng-e <headfat1218@gmail.com>
2020-02-17 18:44:00 +0900

    apis 모듈을 패키지화.

--- DRF_Tutorial/app/snippets/urls.py
+++ DRF_Tutorial/app/snippets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import apis
+from .apis import api_view
 from .views import snippet_list, snippet_detail
 app_name = 'snippets'
 
@@ -9,6 +9,6 @@ urlpatterns = [
     # path('snippets/<int:pk>/', snippet_detail),
 
     # Class-based view 를 사용하는 경우, as_view()함수를 호출
-    path('snippets/', apis.SnippetListCreateAPIView.as_view()),
-    path('snippets/<int:pk>/', apis.SnippetRetrieveUpdateDestroyAPIView.as_view()),
+    path('snippets/', api_view.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>/', api_view.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
