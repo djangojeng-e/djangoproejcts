11032c1ab70befdd1f3c48443a2436e9335ba9aa
djangojeng-e <headfat1218@gmail.com>
2020-02-18 20:43:48 +0900

    Generic Class-based APIView를 사용.

--- DRF_Tutorial/app/snippets/urls.py
+++ DRF_Tutorial/app/snippets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .apis import mixins
+from .apis import generic
 from .views import snippet_list, snippet_detail
 app_name = 'snippets'
 
@@ -9,6 +9,6 @@ urlpatterns = [
     # path('snippets/<int:pk>/', snippet_detail),
 
     # Class-based view 를 사용하는 경우, as_view()함수를 호출
-    path('snippets/', mixins.SnippetListCreateAPIView.as_view()),
-    path('snippets/<int:pk>/', mixins.SnippetRetrieveUpdateDestroyAPIView.as_view()),
+    path('snippets/', generic.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>/', generic.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
