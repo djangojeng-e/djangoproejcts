92a0e73d5173aa260ec052824323bbf332f43da7
djangojeng-e <headfat1218@gmail.com>
2020-02-19 21:59:34 +0900

    Test Case.

--- DRF_Tutorial/app/snippets/tests.py
+++ DRF_Tutorial/app/snippets/tests.py
@@ -49,45 +49,45 @@ class SnippetTest(APITestCase):
                 snippet_data,
             )
 
-        def test_snippet_create(self):
-            """
-            Snippet 객체를 만든다.
-            :param self:
-            :return:
-            """
-            url = '/api-view/snippets/'
+    def test_snippet_create(self):
+        """
+        Snippet 객체를 만든다.
+        :param self:
+        :return:
+        """
+        url = '/api-view/snippets/'
 
-            # Snippet 객체를 만들기 위해 클라이언트로부터 전달될 JSON 객체를 Parse한
-            # Python 객체
+    # Snippet 객체를 만들기 위해 클라이언트로부터 전달될 JSON 객체를 Parse한
+        # Python 객체
 
-            data = {
-                'code': 'def abc():',
-            }
-            response = self.client.post(url, data=data)
-            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = {
+            'code': 'def abc():',
+        }
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-            # 응답에 돌아온 객체가 SnippetSerializer로
-            # 실제 Model instance를 serialize 한 결과와 같은지 확인
-            pk = response.data['pk']
-            snippet = Snippet.objects.get(pk=pk)
-            self.assertEqual(
-                response.data,
-                SnippetSerializer(snippet).data,
-            )
-            # 객체를 하나 생성했으니, 전체 Snippet 객체의 개수가 1개인지 확인 (ORM)
-            self.assertEqual(Snippet.objects.count(), 1)
+        # 응답에 돌아온 객체가 SnippetSerializer로
+        # 실제 Model instance를 serialize 한 결과와 같은지 확인
+        pk = response.data['pk']
+        snippet = Snippet.objects.get(pk=pk)
+        self.assertEqual(
+            response.data,
+            SnippetSerializer(snippet).data,
+        )
+        # 객체를 하나 생성했으니, 전체 Snippet 객체의 개수가 1개인지 확인 (ORM)
+        self.assertEqual(Snippet.objects.count(), 1)
 
-        def test_snippet_delete(self):
-            # 미리 객체를 5개 만들어 놓는다.
-            # delete API를 적절히 실행한후 객체가 4개가 되었는지 확인
-            # 지운 객체가 실제로 존재하는지 확인
-            snippets = [Snippet.objects.create(code='1') for i in range(5)]
-            self.assertEqual(Snippet.objects.count(), 5)
+    def test_snippet_delete(self):
+        # 미리 객체를 5개 만들어 놓는다.
+        # delete API를 적절히 실행한후 객체가 4개가 되었는지 확인
+        # 지운 객체가 실제로 존재하는지 확인
+        snippets = [Snippet.objects.create(code='1') for i in range(5)]
+        self.assertEqual(Snippet.objects.count(), 5)
 
-            snippet = random.choice(snippets)
-            url = f'/api-view/snippets/{snippet.pk}/'
-            response = self.client.delete(url)
+        snippet = random.choice(snippets)
+        url = f'/api-view/snippets/{snippet.pk}/'
+        response = self.client.delete(url)
 
-            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-            self.assertEqual(Snippet.objects.count(), 4)
-            self.assertEqual(Snippet.objects.filter(pk=snippet.pk).exists())
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Snippet.objects.count(), 4)
+        self.assertEqual(Snippet.objects.filter(pk=snippet.pk).exists())
