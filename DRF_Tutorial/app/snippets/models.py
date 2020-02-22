bc66c8162e6a3d391d78c6c8e7608b647ddbe3ff
djangojeng-e <headfat1218@gmail.com>
2020-02-22 13:30:08 +0900

    Snippet.author 필드 추가 및 DB migration수정

--- DRF_Tutorial/app/snippets/models.py
+++ DRF_Tutorial/app/snippets/models.py
@@ -15,6 +15,7 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 class Snippet(models.Model):
     # DB Index 설정
     # created = models.DateTimeField(auto_now_add=True, db_index=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
