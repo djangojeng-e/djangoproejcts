cb9685fb57264db3c0985804d9add63aebfadfa2
djangojeng-e <headfat1218@gmail.com>
2020-02-28 15:55:54 +0900

    Registered Product in admin.

--- online_shop/shop/admin.py
+++ online_shop/shop/admin.py
@@ -14,3 +14,11 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'price', 'stock', 'available_display', 'available_order', 'created', 'updated']
+    list_filter = ['available_display', 'created', 'updated', 'category']
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['price', 'stock', 'available_display', 'available_order']
+    # list_editable enables main values to be edited immediately.
+
+admin.site.register(Product, ProductAdmin)
