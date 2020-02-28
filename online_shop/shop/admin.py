4359d3cc22a231dd1cab93f53f67c22ea37f6c60
djangojeng-e <headfat1218@gmail.com>
2020-02-28 15:50:23 +0900

    Registered Category into Admin.

--- online_shop/shop/admin.py
+++ online_shop/shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+    # prepopulated_fields equates to the name entered in admin.
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
