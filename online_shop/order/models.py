bc509c20f0644a06624769f0a812c383474cda3b
djangojeng-e <headfat1218@gmail.com>
2020-04-02 18:49:18 +0900

    Created the first order model(class name Order).

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+from coupon.models import Coupon
 
 # Create your models here.
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    coupon = models.ForeignKey(Coupon, on_delete=models.PROTECT, related_name='order_coupon', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), - MaxValueValidator(100000)])
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_product(self):
+        return sum(item.get_item_price() for item in self.item.all())
+
+    def get_total_price(self):
+        total_product = self.get_total_product()
+        return total_product - self.discount
+
+
