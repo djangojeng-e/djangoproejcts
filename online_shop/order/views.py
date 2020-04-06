d3b8f4e9a3d60cd791213ab92a6f320489d972fe
djangojeng-e <headfat1218@gmail.com>
2020-04-06 13:59:00 +0900

    Created order_create view in order.

--- online_shop/order/views.py
+++ online_shop/order/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from cart.cart import Cart
+from .forms import *
 
 # Create your views here.
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+
+                cart.clear()
+                return render(request, 'order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', {'cart':cart, 'form':form})
+
+
