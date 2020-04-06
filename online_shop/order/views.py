4c2b7d802a8490e0f7e3ffb889176048a1ad524b
djangojeng-e <headfat1218@gmail.com>
2020-04-06 14:08:35 +0900

    Added OrderCreateAjaxView class view in order app.

--- online_shop/order/views.py
+++ online_shop/order/views.py
@@ -35,3 +35,33 @@ def order_complete(request):
     return render(request, 'order/created.html', {'order': order})
 
 
+from django.views.generic.base import View
+from django.http import JsonResponse
+
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated": False}, status=403)
+
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id": order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
