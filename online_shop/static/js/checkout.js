e6b19dfe1d41d911f7d4ac20a35256b59efefeab
djangojeng-e <headfat1218@gmail.com>
2020-04-06 19:23:26 +0900

    Added first function AjaxCreateOrder in checkout.js

--- online_shop/static/js/checkout.js
+++ online_shop/static/js/checkout.js
@@ -46,3 +46,29 @@ $(function (){
     });
 });
 
+function AjaxCreateOrder(e) {
+    e.preventDefault();
+    var order_id = '';
+    var request = $.ajax({
+        method: "POST",
+        url: order_create_url,
+        async: false,
+        data: $('.order-form').serialize()
+    });
+    request.done(function (data){
+        if (data.order_id) {
+         order_id = data.order_id;
+         }
+
+    });
+    request.fail(function (jqXHR, textStatus){
+        if (jqXHR.status == 404) {
+            alert("페이지가 존재하지 않습니다.");
+        } else if (jqXHR.status == 403) {
+            alert("로그인 해주세요.");
+        } else {
+            alert("문제가 발생했습니다. 다시 시도해주세요.");
+        }
+    });
+    return order_id
+}
