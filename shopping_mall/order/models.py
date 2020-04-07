29f6ad93509180401ea58932ba325a0cb17d3537
djangojeng-e <headfat1218@gmail.com>
2020-04-07 21:08:05 +0900

    Created basic models for fcuser, order, product

--- shopping_mall/order/models.py
+++ shopping_mall/order/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from fcuser.models import Fcuser
+from product.models import Product
 # Create your models here.
+
+
+class Order(models.Model):
+    fcuser = models.ForeignKey(Fcuser, on_delete=models.CASCADE, verbose_name='사용자')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='상품')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날자')
+    quantity = models.IntegerField(verbose_name='수량')
+
