c16ff8b34a262368d46717022eb2f6ee411bb15e
djangojeng-e <headfat1218@gmail.com>
2020-04-07 14:44:12 +0900

    Added def export_to_csv to admin.py in order app.

--- online_shop/order/admin.py
+++ online_shop/order/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+import csv
+import datetime
+from django.http import HttpResponse
+
 # Register your models here.
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # csv 파일 컬럼 타이틀 줄
+    writer.writerow([field.verbose_name for field in fields])
+
+    # 실제 데이터 출력
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%Y-%m-%d")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
