aca0dc625832ffb75a9838d587790b6e1e262463
djangojeng-e <headfat1218@gmail.com>
2020-04-12 04:11:48 +0900

    Created User Model and added to admin in user app.

--- t_story/users/models.py
+++ t_story/users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, username, password, **extra_fields):
+        """
+        Crete and save a user with the given username, email and password. 
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+
+    def create_user(self, email, username='', password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, username, password, **extra_fields)
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email, username, password, **extra_fields)
+
+
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+
+    username = models.CharField(max_length=30)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []        
+    # 필수로 받고싶은 필드를 넣기위해 사용. 로그인을 이메일로 하니까, 빈 리스트로 남겨둠.  
+
+    def __str__(self):
+        return "<%d %s>" % (self.pk, self.email)
+
