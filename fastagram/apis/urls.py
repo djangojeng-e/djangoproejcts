1af2ad7765b3968fabfa2503102534d271c1519f
djangojeng-e <headfat1218@gmail.com>
2020-04-29 12:09:41 +0900

    DRF 튜토리얼 초기환경설정 완료.

--- fastagram/apis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-
-from apis.views import UserCreateView, UserLoginView
-
-urlpatterns = [
-    path('v1/users/create/', UserCreateView.as_view(), name='apis_v1_create'),
-    path('v1/users/login/', UserLoginView.as_view(), name='apis_v1_login'),
-]
