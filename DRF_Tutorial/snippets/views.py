e39330291bd40c9b7743b817b124ebf1d69e4bbc
djangojeng-e <headfat1218@gmail.com>
2020-05-03 19:04:20 +0900

    Tutorial 5 - 하이라이트된 스니펫에 앤드포인트 생성

--- DRF_Tutorial/snippets/views.py
+++ DRF_Tutorial/snippets/views.py
@@ -8,6 +8,12 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
 
 
 @api_view(['GET'])
