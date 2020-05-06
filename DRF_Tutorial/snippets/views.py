b4aff7f83546bfb5f0de69dcee762338e5897f6c
djangojeng-e <headfat1218@gmail.com>
2020-05-06 16:21:09 +0900

    Tutorial 6 - ViewSets to URLs

--- DRF_Tutorial/snippets/views.py
+++ DRF_Tutorial/snippets/views.py
@@ -1,11 +1,20 @@
 from rest_framework import viewsets, renderers
 from .serializers import UserSerializer, SnippetSerializer
 from django.contrib.auth.models import User
-from rest_framework.decorators import action, renderer_classes
+from rest_framework.decorators import action, renderer_classes, api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import permissions
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
