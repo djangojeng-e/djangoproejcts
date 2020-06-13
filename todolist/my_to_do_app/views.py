d7e44638f229a2daf4b65820a7be8bb85c068c6b
djangojeng-e <headfat1218@gmail.com>
2020-06-13 21:49:31 +0900

    index.html and views.py, tested request.POST

--- todolist/my_to_do_app/views.py
+++ todolist/my_to_do_app/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'my_to_do_app/index.html')
 
+def CreateTodo(request):
+    user_input_str = request.POST['todoContent']
+    return HttpResponse("Create todo를 할거야! =>" + user_input_str)
+
