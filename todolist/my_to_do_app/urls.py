adc683bce2afd7b209b7480a36d4a97f09d87e5b
djangojeng-e <headfat1218@gmail.com>
2020-06-13 22:39:46 +0900

    Added create method to views and tested okay

--- todolist/my_to_do_app/urls.py
+++ todolist/my_to_do_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('createTodo/', views.CreateTodo)
+    path('', views.index, name='index'),
+    path('createTodo/', views.CreateTodo, name='CreateTodo')
 ]
--- todolist/my_to_do_app/views.py
+++ todolist/my_to_do_app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import * 
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'my_to_do_app/index.html')
+    todos = Todo.objects.all()
+    content = {'todos': todos}
+    return render(request, 'my_to_do_app/index.html', content)
+
 
 def CreateTodo(request):
     user_input_str = request.POST['todoContent']
-    return HttpResponse("Create todo를 할거야! =>" + user_input_str)
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("Create todo를 할거야! =>" + user_input_str)
+
 
