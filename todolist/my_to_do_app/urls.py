d01fd821a72a45745c17e2a6a478c00c61b43bc3
djangojeng-e <headfat1218@gmail.com>
2020-06-17 13:43:23 +0900

    todolist with django - completed

--- todolist/my_to_do_app/urls.py
+++ todolist/my_to_do_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('createTodo/', views.CreateTodo, name='CreateTodo'),
-    path('deleteTodo/', views.doneTodo, name='deleteTodo'),
+    path('donetTodo/', views.doneTodo, name='doneTodo'),
 ]
